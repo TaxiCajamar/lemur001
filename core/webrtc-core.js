960dc3b8482742b9cb2e639b1cba01b0553c6024
TaxiCajamar <82846690+TaxiCajamar@users.noreply.github.com>
2025-10-26 08:14:04 -0300

    Update webrtc-core.js

--- core/webrtc-core.js
+++ core/webrtc-core.js
@@ -10,6 +10,7 @@ class WebRTCCore {
     this.currentCaller = null;
     this.dataChannel = null;
     this.onDataChannelMessage = null;
+    this.onIncomingCall = null;
 
     window.rtcDataChannel = {
         send: (message) => {
@@ -136,6 +137,7 @@ class WebRTCCore {
 
     this.socket.on('incomingCall', data => {
         this.currentCaller = data.from;
+       window.lastCallerId = data.from;
         if (this.onIncomingCall) {
             this.onIncomingCall(data.offer, data.callerLang);
         }
