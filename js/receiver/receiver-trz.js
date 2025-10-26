f4a6f713693444a86d8fc12b3c592d524adadf32
TaxiCajamar <82846690+TaxiCajamar@users.noreply.github.com>
2025-10-25 23:16:55 -0300

    Update receiver-trz.js

--- js/receiver/receiver-trz.js
+++ js/receiver/receiver-trz.js
@@ -22,7 +22,7 @@ function enviarParaOutroCelular(texto) {
 
 async function translateText(text) {
     try {
-        const response = await fetch('https://chat-tradutor-bvvx.onrender.com/translate', {
+        const response = await fetch('https://chat-tradutor.onrender.com/translate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
