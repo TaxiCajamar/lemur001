8d66608da25ac4e4becbab6010832e27d75b6abe
TaxiCajamar <82846690+TaxiCajamar@users.noreply.github.com>
2025-10-25 23:18:38 -0300

    Update receiver-ui.js

--- js/receiver/receiver-ui.js
+++ js/receiver/receiver-ui.js
@@ -181,7 +181,7 @@ async function obterIdiomaCompleto(lang) {
 // 🌐 Tradução apenas para texto
 async function translateText(text, targetLang) {
     try {
-        const response = await fetch('https://chat-tradutor-bvvx.onrender.com/translate', {
+        const response = await fetch('https://chat-tradutor.onrender.com/translate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ text, targetLang })
