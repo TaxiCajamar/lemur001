64dc64a3cb18ac3da5fbdae66b16f54674fcabfd
TaxiCajamar <82846690+TaxiCajamar@users.noreply.github.com>
2025-10-30 16:56:29 -0300

    Update internet-config.js

--- core/internet-config.js
+++ core/internet-config.js
@@ -1,30 +1,13 @@
-// core/internet-config.js
 export const SIGNALING_SERVER_URL = 'https://lemur-signal.onrender.com';
 
 export const getIceServers = () => {
   return [
-    // STUN servers
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
-    
-    // TURN servers alternativos
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
-    },
-    {
-      urls: 'turn:openrelay.metered.ca:443',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
-    },
-    {
-      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turn:meet-jit-si-turnrelay.jitsi.net:443?transport=tcp',
+      username: 'guest',
+      credential: 'guest'
     }
   ];
 };
